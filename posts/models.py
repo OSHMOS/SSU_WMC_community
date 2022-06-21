0bb92fefe0bab3423945c7baff7e19d2f5b9aa11
SEUNGHYUN OH <seunghyunoh@SEUNGHYUNui-MacBookAir.local>
2022-06-21 20:10:37 +0900

    YB 게시판 글 모델 테이블 생성하기

--- posts/models.py
+++ posts/models.py
@@ -4,4 +4,17 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     pass
-  
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        related_name='author'
+    )
+    dt_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
