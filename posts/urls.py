4deb64a48b25afb089b100d87cfcf869c056feda
SEUNGHYUN OH <seunghyunoh@SEUNGHYUNui-MacBookAir.local>
2022-06-22 18:10:48 +0900

    게시판 게시글 수정 기능 구현하기

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   path('', views.index, name='index'),
   path('detail/<int:post_id>', views.post_detail, name='post_detail'),
   path('create/', views.post_create, name='post_create'),
+  path('update/<int:post_id>', views.post_update, name='post_update'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -29,3 +29,24 @@ def post_create(request):
         form = Postform()
     ctx = {'form' : form}
     return render(request, 'posts/post_form.html', ctx)
+
+
+@login_required(login_url='account_login')
+def post_update(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    
+    if request.method == "POST":
+        form = Postform(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', post.id)
+    else:
+        if post.author == request.user:
+            form = Postform(instance=post)
+        else:
+            ctx = {'post' : post}
+            return render(request, 'posts/post_not_access.html', ctx)
+    ctx = {'form' : form}
+    return render(request, 'posts/post_update.html', ctx)
