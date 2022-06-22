4beaa2e5e163eb9fb75d57719a323e14e36f066d
SEUNGHYUN OH <seunghyunoh@SEUNGHYUNui-MacBookAir.local>
2022-06-22 17:48:33 +0900

    게시판 로그인, 회원가입, 목록 페이지 구현하기

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):
+    posts = Post.objects.all()
+    ctx = {'posts' : posts}
     return render(request, 'posts/index.html')
