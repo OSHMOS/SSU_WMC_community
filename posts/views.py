c1b1a8b84081fc1d68b4d5023545c1c38d72b74b
SEUNGHYUN OH <seunghyunoh@SEUNGHYUNui-MacBookAir.local>
2022-06-24 20:33:41 +0900

    게시판 페이지 페이지네이터 구헌하기(템플릿 태그 추가하기)

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from .models import Post
 from .forms import Postform
 
