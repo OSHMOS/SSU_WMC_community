43aa3c620a765b49531ae41a25aa51521b6dee63
SEUNGHYUN OH <seunghyunoh@SEUNGHYUNui-MacBookAir.local>
2022-06-27 22:01:18 +0900

    게시글 공감 기능 적용하기

--- posts/models.py
+++ posts/models.py
@@ -8,6 +8,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='post_author')
     dt_created = models.DateTimeField(auto_now_add=True)
     dt_updated = models.DateTimeField(null=True, blank=True)
+    like = models.ManyToManyField(User, blank=True, related_name='post_like')
+    bookmark = models.ManyToManyField(User, blank=True, related_name='post_bookmark')
 
     def __str__(self):
         return self.title
@@ -19,6 +21,7 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='comment_author')
     dt_created = models.DateTimeField(auto_now_add=True)
     dt_updated = models.DateTimeField(null=True, blank=True)
+    like = models.ManyToManyField(User, related_name='comment_like')
 
     def __str__(self):
         return self.content
--- posts/views.py
+++ posts/views.py
@@ -82,6 +82,23 @@ def post_delete(request, post_id):
 
 
 @login_required(login_url='account_login')
+def post_like(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.user == post.author:
+        messages.error(request, '본인이 작성한 글은 공감할 수 없습니다.')
+    else:
+        post.like.add(request.user)
+    return redirect('posts:post_detail', post_id)
+
+
+@login_required(login_url='account_login')
+def post_like_cancel(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    post.like
+    return redirect('posts:post_detail', post_id)
+
+
+@login_required(login_url='account_login')
 def comment_create(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     
